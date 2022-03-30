ae0007f908e272603f1cc313cd2236a765ae18bf
Trenten <trenten.grede@gmail.com>
2022-03-30 18:04:40 -0500

    updated variables to have MBTC and MBTC_UST and made uppercase

--- src/utilities/variables.tsx
+++ src/utilities/variables.tsx
@@ -1,4 +1,6 @@
-export const discord = 'https://discord.gg/yfd';
-export const telegram = 'https://t.me/yfoundrydao';
-export const twitter = 'https://twitter.com/yieldfoundrydao';
-export const medium = 'https://medium.com/@yfoundry/';
+export const DISCORD = 'https://discord.gg/yfd';
+export const TELEGRAM = 'https://t.me/yfoundrydao';
+export const TWITTER = 'https://twitter.com/yieldfoundrydao';
+export const MEDIUM = 'https://medium.com/@yfoundry/';
+export const MBTC = 'terra1csr22xvxs6r3gkjsl7pmjkmpt39mwjsrm0e2r8';
+export const MBTC_UST = 'terra134jl4dt20mqfryhnmhauryr754vuw7990jdell';
