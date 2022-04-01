20b7234a836451e33adfac9e0231550d6adde326
Trenten <trenten.grede@gmail.com>
2022-03-31 19:42:16 -0500

    Extracted the instantiateMsg into its own file to clean things up

---
+++ src/utilities/instantiateMsg.tsx
@@ -0,0 +1,15 @@
+const instantiateMsg = {
+  anchor_money_market: {
+    contract_addr: 'terra15dwd5mj8v59wpj0wvt233mf5efdff808c5tkal',
+    deposit_coin_denom: 'uusd',
+    minted_asset_contract_addr: 'terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl'
+  },
+  mirror_mint: {
+    contract_addr: 'terra1s9ehcjv0dqj2gsl72xrpp0ga5fql7fj7y3kq3w'
+  },
+  mirror_oracle: {
+    contract_addr: 'terra1uvxhec74deupp47enh7z5pk55f3cvcz8nj4ww9'
+  }
+};
+
+export default instantiateMsg;
