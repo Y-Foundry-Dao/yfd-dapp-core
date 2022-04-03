b0ef5897c62d0a5912cb5ff96311386df6ed46b0
Trenten <trenten.grede@gmail.com>
2022-04-02 20:48:04 -0500

    Refactored the app file and the custom hooks

--- src/utilities/hooks/useContract.tsx
+++ src/utilities/hooks/useContract.tsx
@@ -1,22 +1,41 @@
-import { useWallet, TxResult } from '@terra-money/wallet-provider';
 import { useState } from 'react';
-import { Msg } from '@terra-money/terra.js';
+
+import {
+  useWallet,
+  TxResult,
+  ConnectedWallet
+} from '@terra-money/wallet-provider';
+import { Coins, Msg, MsgExecuteContract } from '@terra-money/terra.js';
 
 const useContract = () => {
   const { post } = useWallet();
+  const [txHashFromExecute, setTxHashFromExecute] = useState('');
 
-  const executeMsg = async (msg: Msg) => {
+  const executeMsg = async (
+    connectedWallet: ConnectedWallet,
+    contractAddress: string,
+    msgQuery: object,
+    amount: Coins.Input = {}
+  ) => {
     try {
-      const result: TxResult = await post({ msgs: [msg] });
+      const msg: Msg = new MsgExecuteContract(
+        connectedWallet.walletAddress,
+        contractAddress,
+        msgQuery,
+        amount
+      );
 
-      return result.result.txhash;
+      const result: TxResult = await post({ msgs: [msg] });
+      return setTxHashFromExecute(result.result.txhash);
     } catch (error) {
       console.log(error);
     }
   };
 
   return {
-    executeMsg
+    executeMsg,
+    txHashFromExecute,
+    setTxHashFromExecute
   };
 };
 
