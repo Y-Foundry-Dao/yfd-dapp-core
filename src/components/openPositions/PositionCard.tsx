a722bd6b6f1d0cb0dfcf81d87dd6dac096d94c11
Trenten <trenten.grede@gmail.com>
2022-04-23 01:38:02 -0500

    Removed import for msgAddToPosition

--- src/components/openPositions/PositionCard.tsx
+++ src/components/openPositions/PositionCard.tsx
@@ -1,7 +1,6 @@
 import InputAmount from 'components/depositModal/input/InputAmount';
 import DepositButton from 'components/buttons/basic/Button';
 import { useEffect, useState } from 'react';
-import msgAddToPosition from 'utilities/messagesExecute/msgAddToPosition';
 import useContract from 'utilities/hooks/useContractDGSF';
 import { useConnectedWallet } from '@terra-money/wallet-provider';
 import styled from 'styled-components';
