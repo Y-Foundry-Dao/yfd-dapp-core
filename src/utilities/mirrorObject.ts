5486141d74b8605db3a816c94ac9f836118abd52
Trenten <trenten.grede@gmail.com>
2022-05-03 08:09:26 -0500

    Created mirror object that holds the testnet contract address of massets

---
+++ src/utilities/mirrorObject.ts
@@ -0,0 +1,20 @@
+const mirror = {
+  aUST: 'terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl',
+  mAAPL: 'terra16vfxm98rxlc8erj4g0sj5932dvylgmdufnugk0',
+  mABNB: 'terra1avryzxnsn2denq7p2d7ukm6nkck9s0rz2llgnc',
+  mAMZN: 'terra12saaecsqwxj04fn0jsv4jmdyp6gylptf5tksge',
+  mBABA: 'terra15dr4ah3kha68kam7a907pje9w6z2lpjpnrkd06',
+  mBTC: 'terra1csr22xvxs6r3gkjsl7pmjkmpt39mwjsrm0e2r8',
+  mCOIN: 'terra1qre9crlfnulcg0m68qqywqqstplgvrzywsg3am',
+  mETH: 'terra1ys4dwwzaenjg2gy02mslmc96f267xvpsjat7gx',
+  mFB: 'terra14gq9wj0tt6vu0m4ec2tkkv4ln3qrtl58lgdl2c',
+  mGME: 'terra104tgj4gc3pp5s240a0mzqkhd3jzkn8v0u07hlf',
+  mGOOGL: 'terra1qg9ugndl25567u03jrr79xur2yk9d632fke3h2',
+  mMSFT: 'terra12s2h8vlztjwu440khpc0063p34vm7nhu25w4p9',
+  mQQQ: 'terra18yx7ff8knc98p07pdkhm3u36wufaeacv47fuha',
+  mSLV: 'terra1fdkfhgk433tar72t4edh6p6y9rmjulzc83ljuw',
+  mTSLA: 'terra1nslem9lgwx53rvgqwd8hgq7pepsry6yr3wsen4',
+  mUSO: 'terra1fucmfp8x4mpzsydjaxyv26hrkdg4vpdzdvf647'
+};
+
+export default mirror;
