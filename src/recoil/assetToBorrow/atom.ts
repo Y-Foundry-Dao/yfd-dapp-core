a57948f95201e088478c237f3b2891e75e51e4f1
Trenten <trenten.grede@gmail.com>
2022-05-07 09:23:53 -0500

    Added atom for default assetToBorrow being mBTC

---
+++ src/recoil/assetToBorrow/atom.ts
@@ -0,0 +1,8 @@
+import { atom } from 'recoil';
+
+const assetToBorrowAtom = atom({
+  key: 'assetToBorrowAtom',
+  default: 'terra1csr22xvxs6r3gkjsl7pmjkmpt39mwjsrm0e2r8'
+});
+
+export default assetToBorrowAtom;
