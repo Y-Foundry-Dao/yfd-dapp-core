4be0f8ef1ce0432610aee1c71bf004ad8fc0a0f1
Trenten <trenten.grede@gmail.com>
2022-05-08 07:46:20 -0500

    Added tokenFactory query so we can get asset_pair info

--- src/utilities/variables.tsx
+++ src/utilities/variables.tsx
@@ -8,3 +8,4 @@ export const MSPY = 'terra15t9afkpj0wnh8m74n8n2f8tspkn7r65vnru45s';
 export const MSPY_UST = 'terra10q4qsd2sfl4wc6nv0d77zud4ur40ygf5602pzd';
 export const AUST = 'terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl';
 export const CONTRACT_REGISTRY = 'terra16gg7alyat08s99rafzdwvnpmjcxl4s3kww3cyq';
+export const TOKEN_FACTORY = 'terra18qpjm4zkvqnpjpw0zn0tdr8gdzvt8au35v45xf';
