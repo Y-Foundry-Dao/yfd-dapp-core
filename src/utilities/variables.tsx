35efc9f93d17a82d37297116e89d16530cb9a9b2
Trenten <trenten.grede@gmail.com>
2022-09-26 01:34:19 -0500

    added yfd and forge contracts to variables

--- src/utilities/variables.tsx
+++ src/utilities/variables.tsx
@@ -9,3 +9,7 @@ export const MSPY_UST = 'terra10q4qsd2sfl4wc6nv0d77zud4ur40ygf5602pzd';
 export const AUST = 'terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl';
 export const CONTRACT_REGISTRY = 'terra16gg7alyat08s99rafzdwvnpmjcxl4s3kww3cyq';
 export const TOKEN_FACTORY = 'terra18qpjm4zkvqnpjpw0zn0tdr8gdzvt8au35v45xf';
+export const FORGE_TEST =
+  'terra1pmayk6e2zt78hlmkwyk6qxfhu390urw6prkd46jc9e7d8k45wq3sldycwz';
+export const YFD_TEST =
+  'terra1ss9zz4873vk4dd8dvua0vm83m6s8k7ctwp9efac0arytn6jthfgsy2d4a9';
