e2ff4fc926ae94e4eb702a272d307064a1eaf554
Trenten <trenten.grede@gmail.com>
2022-09-26 01:33:27 -0500

    Updated LCD to new terra testnet

--- src/utilities/lcd.tsx
+++ src/utilities/lcd.tsx
@@ -1,6 +1,6 @@
 import { LCDClient } from '@terra-money/terra.js';
 
 export const terra = new LCDClient({
-  URL: 'https://bombay-lcd.terra.dev',
-  chainID: 'bombay-12'
+  URL: 'https://pisco-lcd.terra.dev',
+  chainID: 'pisco-1'
 });
