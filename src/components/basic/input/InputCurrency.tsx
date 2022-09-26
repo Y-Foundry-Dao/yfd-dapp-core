ec9a5dac9997070b1b29459c610f4024922eb430
Trenten <trenten.grede@gmail.com>
2022-09-26 13:53:24 -0500

    Created an inputCurrency compnent that builds positive number only input

---
+++ src/components/basic/input/InputCurrency.tsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import CurrencyInput from 'react-currency-input-field';
+import amountDepositYFDAtom from 'recoil/amountDepositYFD/atom';
+
+interface Props {
+  id: string;
+  name: string;
+  placeholder: string;
+  decimalsLimit: number;
+  amount: number;
+  setAmount: (value: number) => void;
+  label: string;
+}
+
+function InputCurrency({
+  id,
+  name,
+  placeholder,
+  decimalsLimit,
+  amount,
+  setAmount,
+  label
+}: Props) {
+  const handleValueChange = (value: any) => {
+    value !== typeof String && setAmount(Number(value) || 0);
+  };
+
+  return (
+    <Label>
+      <StyledTitle>{label}</StyledTitle>
+      <CurrencyInput
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        allowNegativeValue={false}
+        decimalsLimit={decimalsLimit}
+        onValueChange={handleValueChange}
+        value={amount}
+      />
+    </Label>
+  );
+}
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 2%;
+`;
+
+const StyledTitle = styled.h2`
+  text-shadow: 1px 3px 6px black, 0 0 0 gray, 1px 4px 2px #333;
+  margin: 15% 0 0 0;
+  padding-bottom: 0px;
+  font-size: 1em;
+  color: ${(props) => `${props.theme.colors.color3}`};
+  display: grid;
+  grid-template-columns: 1fr max-content 1fr;
+  grid-template-rows: 21px;
+  grid-gap: 10px;
+  align-items: center;
+
+  :after,
+  :before {
+    content: ' ';
+    display: block;
+    border-bottom: 1px solid ${(props) => `${props.theme.colors.color3}`};
+    box-shadow: 0 6px 7px -7px ${(props) => `${props.theme.colors.color1}`};
+    height: 5px;
+  }
+`;
+
+export default InputCurrency;
