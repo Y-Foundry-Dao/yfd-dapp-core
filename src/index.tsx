e1689a5aff6d4617f1bcfba59b045f0e137d6af0
Trenten <trenten.grede@gmail.com>
2022-09-27 23:33:29 -0500

    changed to saas provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { getChainOptions, WalletProvider } from '@terra-money/wallet-provider';
 import { ChakraProvider } from '@chakra-ui/react';
+import { SaasProvider } from '@saas-ui/react';
 
 import App from 'App';
 import GlobalCss from 'styles/global.css';
@@ -14,12 +15,12 @@ getChainOptions().then((chainOptions) => {
   root.render(
     <WalletProvider {...chainOptions}>
       {/* <ThemeComponent> */}
-      <ChakraProvider>
+      <SaasProvider>
         {/* <GlobalCss /> */}
         <RecoilRoot>
           <App />
         </RecoilRoot>
-      </ChakraProvider>
+      </SaasProvider>
       {/* </ThemeComponent> */}
     </WalletProvider>
   );
