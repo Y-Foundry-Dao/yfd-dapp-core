12a3cacf1f91e298d3cba4b8fa088031b082d844
Trenten <trenten.grede@gmail.com>
2022-09-29 12:41:53 -0500

    Fixed contract address and renamed old one with old suffix

--- src/utilities/variables.tsx
+++ src/utilities/variables.tsx
@@ -2,7 +2,9 @@ export const DISCORD = 'https://discord.gg/yfd';
 export const TELEGRAM = 'https://t.me/yfoundrydao';
 export const TWITTER = 'https://twitter.com/yieldfoundrydao';
 export const MEDIUM = 'https://medium.com/@yfoundry/';
-export const FORGE_TEST =
+export const FORGE_TEST_OLD =
   'terra1pmayk6e2zt78hlmkwyk6qxfhu390urw6prkd46jc9e7d8k45wq3sldycwz';
 export const YFD_TEST =
   'terra1ss9zz4873vk4dd8dvua0vm83m6s8k7ctwp9efac0arytn6jthfgsy2d4a9';
+export const FORGE_TEST =
+  'terra168rvweaknfd7dhde02ex66fjsm9e3xppkj4xskvtnuql4vwr0ptsy444dx';
