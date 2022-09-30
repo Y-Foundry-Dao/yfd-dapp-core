19997ccb5b6f7907002c0e338cfc90a69a9447d8
Trenten <trenten.grede@gmail.com>
2022-09-29 23:55:20 -0500

    Updated finder link

--- src/components/basic/toast/FinderLink.tsx
+++ src/components/basic/toast/FinderLink.tsx
@@ -2,6 +2,9 @@ import { Link } from '@chakra-ui/react';
 import { Web3Address } from '@saas-ui/web3';
 
 function FinderLink({ txHash }: any) {
+  if (!txHash) {
+    return null;
+  }
   return (
     <Link isExternal href={`https://finder.terra.money/testnet/tx/${txHash}`}>
       TxHash: <Web3Address address={txHash} />
