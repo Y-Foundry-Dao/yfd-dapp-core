98f31f8f5ed1bdec414fb79ea2fe2d1a5545d19f
Trenten <trenten.grede@gmail.com>
2022-10-01 00:30:48 -0500

    Renamed FinderLink to FinderTxLink for clarity since I will add another

--- src/components/basic/finder/FinderTxLink.tsx
+++ src/components/basic/finder/FinderTxLink.tsx
@@ -1,10 +1,11 @@
 import { Link } from '@chakra-ui/react';
 import { Web3Address } from '@saas-ui/web3';
 
-function FinderLink({ txHash }: any) {
+function FinderTxLink({ txHash }: any) {
   if (!txHash) {
     return null;
   }
+  //finder.terra.money/testnet/address/terra1l40t9unus08y00h4hjtvmfspkzl5v8lz2a9k0racxaxjq4vsudvsq2emjw
   return (
     <Link isExternal href={`https://finder.terra.money/testnet/tx/${txHash}`}>
       TxHash: <Web3Address address={txHash} />
@@ -12,4 +13,4 @@ function FinderLink({ txHash }: any) {
   );
 }
 
-export default FinderLink;
+export default FinderTxLink;
