4dc6760cb2f23b300ea332d692663b3446658db1
Ray Raspberry <ray@yfoundry.io>
2022-10-04 14:10:04 -0500

    Removed Storybook, upgraded libraries

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,11 @@
 const { ProvidePlugin } = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
   config.resolve.fallback = {
+    url: require.resolve('url'),
+    assert: require.resolve('assert'),
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
     buffer: require.resolve('buffer')
