82ae2b3db8154669108a682e531ebed948e10a7b
Ray Raspberry <ray@yfoundry.io>
2022-10-06 02:00:49 -0500

    Deposit by Date.  This will be obsolete.  Deposit by multiplier is more useful

--- src/components/pageBody/stake/StakeYFD.tsx
+++ src/components/pageBody/stake/StakeYFD.tsx
@@ -25,7 +25,29 @@ import {
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import yLogo from 'assets/yfd/logo-orange.svg';
-import { DEFAULT_YFD_LOCK_DURATION } from 'utilities/variables';
+import {
+  DATE_TWO_WEEK,
+  DATE_ONE_MONTH,
+  DATE_TWO_MONTH,
+  DATE_THREE_MONTH,
+  DATE_SIX_MONTH,
+  DATE_NINE_MONTH,
+  DATE_EIGHTEEN_MONTH,
+  DATE_ONE_YEAR,
+  DATE_TWO_YEAR,
+  DEFAULT_YFD_LOCK_DURATION,
+  CHAIN_BLOCK_ONE_YEAR,
+  CHAIN_BLOCK_EIGHTEEN_MONTH,
+  CHAIN_BLOCK_TWO_YEAR,
+  CHAIN_BLOCK_TWO_WEEK,
+  CHAIN_BLOCK_ONE_MONTH,
+  CHAIN_BLOCK_TWO_MONTH,
+  CHAIN_BLOCK_THREE_MONTH,
+  CHAIN_BLOCK_SIX_MONTH,
+  CHAIN_BLOCK_NINE_MONTH,
+  DATE_FYFD_YFD_LOCK_VALUE_PARITY,
+  CHAIN_BLOCK_FYFD_YFD_LOCK_VALUE_PARITY
+} from 'utilities/variables';
 
 function StakeYFD() {
   const [amountDepositYFD, setAmountDepositYFD] =
@@ -75,27 +97,85 @@ function StakeYFD() {
               defaultValue="{DEFAULT_YFD_LOCK_DURATION}"
               title="Duration"
             >
-              <MenuItem>2 Weeks</MenuItem>
-              <MenuItem>1 Month</MenuItem>
-              <MenuItem>3 Months</MenuItem>
-              <MenuItem>6 Months</MenuItem>
-              <MenuItem>9 Months</MenuItem>
               <MenuItem
-                value="5256000"
+                value={CHAIN_BLOCK_TWO_WEEK}
                 onClick={(event) =>
-                  setDurationDepositYFD(event.currentTarget.value)
+                  setDurationDepositYFD(Number(event.currentTarget.value))
                 }
               >
-                12 Months
+                {String(DATE_TWO_WEEK)}
               </MenuItem>
-              <MenuItem>18 Months</MenuItem>
               <MenuItem
-                value="10512000"
+                value={CHAIN_BLOCK_ONE_MONTH}
                 onClick={(event) =>
-                  setDurationDepositYFD(event.currentTarget.value)
+                  setDurationDepositYFD(Number(event.currentTarget.value))
                 }
               >
-                2 Years - {durationDepositYFD} blocks
+                {String(DATE_ONE_MONTH)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_TWO_MONTH}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_TWO_MONTH)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_THREE_MONTH}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_THREE_MONTH)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_SIX_MONTH}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_SIX_MONTH)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_NINE_MONTH}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_NINE_MONTH)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_FYFD_YFD_LOCK_VALUE_PARITY}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                <b>{String(DATE_FYFD_YFD_LOCK_VALUE_PARITY)}</b>
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_ONE_YEAR}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_ONE_YEAR)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_EIGHTEEN_MONTH}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_EIGHTEEN_MONTH)}
+              </MenuItem>
+              <MenuItem
+                value={CHAIN_BLOCK_TWO_YEAR}
+                onClick={(event) =>
+                  setDurationDepositYFD(Number(event.currentTarget.value))
+                }
+              >
+                {String(DATE_TWO_YEAR)}
               </MenuItem>
             </MenuOptionGroup>
           </MenuList>
--- src/utilities/variables.tsx
+++ src/utilities/variables.tsx
@@ -1,7 +1,10 @@
+//social media
 export const DISCORD = 'https://discord.gg/yfd';
 export const TELEGRAM = 'https://t.me/yfoundrydao';
 export const TWITTER = 'https://twitter.com/yieldfoundrydao';
 export const MEDIUM = 'https://medium.com/@yfoundry/';
+
+//contracts
 export const FORGE_TEST_OLD =
   'terra1pmayk6e2zt78hlmkwyk6qxfhu390urw6prkd46jc9e7d8k45wq3sldycwz';
 export const YFD_TEST =
@@ -9,4 +12,77 @@ export const YFD_TEST =
 export const FORGE_TEST =
   'terra168rvweaknfd7dhde02ex66fjsm9e3xppkj4xskvtnuql4vwr0ptsy444dx';
 
-export const DEFAULT_YFD_LOCK_DURATION = '10512000';
+// cronological data
+//export const CRONO_SECONDS_IN_TWO_YEAR = 31556952 * 2; // 63113904 // actual seconds
+export const CRONO_SECONDS_IN_TWO_WEEK = 60 * 60 * 24 * 7 * 2;
+export const CRONO_SECONDS_IN_ONE_MONTH = CRONO_SECONDS_IN_TWO_WEEK * 2;
+export const CRONO_SECONDS_IN_THREE_MONTH = CRONO_SECONDS_IN_ONE_MONTH * 3;
+export const CRONO_SECONDS_IN_SIX_MONTH = CRONO_SECONDS_IN_ONE_MONTH * 6;
+export const CRONO_SECONDS_IN_ONE_YEAR = CRONO_SECONDS_IN_ONE_MONTH * 12;
+export const CRONO_SECONDS_IN_EIGHTEEN_MONTH = CRONO_SECONDS_IN_ONE_MONTH * 18;
+export const CRONO_SECONDS_IN_TWO_YEAR = 63072000; // what we're using
+export const CRONO_SECONDS_YFD_MAX_LOCK = CRONO_SECONDS_IN_TWO_YEAR;
+
+// date text
+export const DATE_TWO_WEEK = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK
+);
+export const DATE_ONE_MONTH = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 2
+);
+export const DATE_TWO_MONTH = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 4
+);
+export const DATE_THREE_MONTH = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 6
+);
+export const DATE_SIX_MONTH = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 12
+);
+export const DATE_NINE_MONTH = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 18
+);
+export const DATE_EIGHTEEN_MONTH = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 36
+);
+export const DATE_ONE_YEAR = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_WEEK * 24
+);
+export const DATE_TWO_YEAR = new Date(
+  Date.now() + 1000 * CRONO_SECONDS_IN_TWO_YEAR
+);
+
+// blockchain specific variables
+export const CHAIN_SECONDS_PER_BLOCK = 6;
+export const CHAIN_BLOCK_TWO_WEEK =
+  CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK;
+export const CHAIN_BLOCK_ONE_MONTH =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 2;
+export const CHAIN_BLOCK_TWO_MONTH =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 4;
+export const CHAIN_BLOCK_THREE_MONTH =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 6;
+export const CHAIN_BLOCK_SIX_MONTH =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 12;
+export const CHAIN_BLOCK_NINE_MONTH =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 18;
+export const CHAIN_BLOCK_EIGHTEEN_MONTH =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 36;
+export const CHAIN_BLOCK_ONE_YEAR =
+  (CRONO_SECONDS_IN_TWO_WEEK / CHAIN_SECONDS_PER_BLOCK) * 24;
+export const CHAIN_BLOCK_TWO_YEAR =
+  CRONO_SECONDS_IN_TWO_YEAR / CHAIN_SECONDS_PER_BLOCK;
+export const CHAIN_BLOCK_YFD_MAX_LOCK = CHAIN_BLOCK_TWO_YEAR; //10512000
+
+// YFD locking data
+export const FYFD_MULTIPLIER_MAX_LOCK = 2.5;
+export const CRONO_FYFD_YFD_LOCK_VALUE_PARITY =
+  1 / (FYFD_MULTIPLIER_MAX_LOCK / CRONO_SECONDS_YFD_MAX_LOCK);
+export const DATE_FYFD_YFD_LOCK_VALUE_PARITY = new Date(
+  Date.now() + 1000 * CRONO_FYFD_YFD_LOCK_VALUE_PARITY
+);
+export const CHAIN_BLOCK_FYFD_YFD_LOCK_VALUE_PARITY =
+  CRONO_FYFD_YFD_LOCK_VALUE_PARITY / CHAIN_SECONDS_PER_BLOCK;
+
+// default values
+export const DEFAULT_YFD_LOCK_DURATION = CHAIN_BLOCK_NINE_MONTH; // for UI use only
