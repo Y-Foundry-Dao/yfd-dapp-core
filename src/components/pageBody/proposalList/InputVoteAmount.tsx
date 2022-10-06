3c83b4c28975d3784665d0c743ddc5ef18ac0bbc
Trenten <trenten.grede@gmail.com>
2022-10-05 22:22:06 -0500

    Added conditional checks for voteTokenBalance to display certain views

--- src/components/pageBody/proposalList/InputVoteAmount.tsx
+++ src/components/pageBody/proposalList/InputVoteAmount.tsx
@@ -17,25 +17,29 @@ function InputVoteAmount({
   const handleInputVoteAmount = (value: any) => setInputVoteTokenAmount(value);
   return (
     <Flex alignItems="center" gap={5}>
-      <Heading as="h3" size="md">
-        Vote Token Amount:
-      </Heading>
+      {voteTokenBalance.balance > 0 ? (
+        <>
+          <Heading as="h3" size="md">
+            Vote Token Amount:
+          </Heading>
 
-      <NumberInput
-        maxW="140px"
-        mr="2rem"
-        defaultValue={0}
-        min={0}
-        max={convertFromBase(voteTokenBalance.balance)}
-        value={inputVoteTokenAmount}
-        onChange={handleInputVoteAmount}
-      >
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
+          <NumberInput
+            maxW="140px"
+            mr="2rem"
+            defaultValue={0}
+            min={0}
+            max={convertFromBase(voteTokenBalance.balance)}
+            value={inputVoteTokenAmount}
+            onChange={handleInputVoteAmount}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </>
+      ) : null}
     </Flex>
   );
 }
