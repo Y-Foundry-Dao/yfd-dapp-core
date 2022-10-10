fb42d8c604de74e2a754cbf2b4b5eecd403d9afb
Trenten <trenten.grede@gmail.com>
2022-10-09 21:56:58 -0500

    Created useStake hook to use stakeInformation and produce useful values

---
+++ src/hooks/useStake.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import {
+  addSeconds,
+  format,
+  formatDuration,
+  fromUnixTime,
+  intervalToDuration,
+  Duration
+} from 'date-fns';
+import { CHAIN_SECONDS_PER_BLOCK } from 'utilities/variables';
+import convertFromBase from 'utilities/converters/convertFromBase';
+
+// Takes stake information from the stakeYFD list and produces useful values
+const useStake = ({ stake }: any) => {
+  const [timeUntilUnlock, setTimeUntilUnlock] = useState<Duration>({});
+
+  const depositTimestampInNanoSeconds = Number(stake.stake.deposit_timestamp);
+  const depositTimestampInSeconds = Math.floor(
+    depositTimestampInNanoSeconds / 1000000000
+  );
+
+  const depositDateInSeconds = fromUnixTime(depositTimestampInSeconds);
+  const depositDateFormatted = format(depositDateInSeconds, 'PPPPpp');
+
+  const lockDuration = stake.stake.lock_duration;
+  const lockDurationInSeconds = lockDuration * CHAIN_SECONDS_PER_BLOCK;
+
+  const unlockDateInSeconds = addSeconds(
+    depositDateInSeconds,
+    lockDurationInSeconds
+  );
+  const unlockDateFormatted = format(unlockDateInSeconds, 'PPPPpp');
+
+  const depositInterval = intervalToDuration({
+    start: depositDateInSeconds,
+    end: unlockDateInSeconds
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTimeUntilUnlock = intervalToDuration({
+        start: Date.now(),
+        end: unlockDateInSeconds
+      });
+      setTimeUntilUnlock(newTimeUntilUnlock);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const lockDurationFormatted = formatDuration(depositInterval);
+  const timeUntilUnlockFormatted = formatDuration(timeUntilUnlock);
+
+  const amountStaked = convertFromBase(
+    stake.stake.asset_deposit_amount
+  ).toFixed(4);
+  return {
+    timeUntilUnlock,
+    depositDateFormatted,
+    unlockDateFormatted,
+    lockDurationFormatted,
+    timeUntilUnlockFormatted,
+    amountStaked
+  };
+};
+
+export default useStake;
