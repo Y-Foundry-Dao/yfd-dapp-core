d0779adfa257d606d517cd5c11cb6fca5892af55
Ray Raspberry <ray@yfoundry.io>
2022-10-11 01:51:08 -0500

    cleaned up locking menu

--- src/components/pageBody/stake/stakedYFD/StakedYFD.tsx
+++ src/components/pageBody/stake/stakedYFD/StakedYFD.tsx
@@ -2,18 +2,23 @@ import { Box } from '@chakra-ui/react';
 import { useRecoilValue } from 'recoil';
 import stakedYFDAtom from 'recoil/stakedYFD/atom';
 import StakeItem from './StakeItem';
-
+import { Accordion, AccordionPanel, AccordionIcon, AccordionButton, AccordionItem } from '@chakra-ui/react';
 function StakedYFD() {
   const stakedYFD = useRecoilValue(stakedYFDAtom);
   return (
-    <div>
-      {stakedYFD.map((stake: any) => {
-        return (
-          <Box key={stake.idx}>
-            <StakeItem stake={stake} />
-          </Box>
-        );
-      })}
+    <Accordian>
+      <AccordionItem>
+        <AccordionButton>
+          <div>
+          {stakedYFD.map((stake: any) => {
+            return (
+              <Box key={stake.idx}>
+                <StakeItem stake={stake} />
+              </Box>
+            );
+          })}
+      </AccordionItem>
+    </Accordian>
     </div>
   );
 }
