f60f37095fab118e3226eac2119812a4727187b3
Trenten <trenten.grede@gmail.com>
2022-10-14 13:34:49 -0500

    mapped emergencies to display in the UI

--- src/components/pageBody/proposalList/ProposalList.tsx
+++ src/components/pageBody/proposalList/ProposalList.tsx
@@ -2,9 +2,10 @@ import ProposalInfo from './proposalInfo/ProposalInfo';
 import { Box, Divider, Flex, Heading } from '@chakra-ui/react';
 import FinderContractLink from 'components/basic/finder/FinderContractLink';
 import useContractForge from 'hooks/useContractForge';
+import EmergencyProposal from './emergencyList/EmergencyProposal';
 
 function ProposalList() {
-  const { proposals } = useContractForge();
+  const { proposals, emergencies } = useContractForge();
 
   return (
     <>
@@ -32,6 +33,25 @@ function ProposalList() {
                     proposalContract={proposal.addr}
                     proposalIndex={proposal.index}
                   />
+                </Box>
+              );
+            })}
+          </Flex>
+          <Heading as="h2" size="lg">
+            Current Emergencies
+          </Heading>
+          <Flex wrap="wrap" gap={5} justifyContent="center">
+            {emergencies.map((emergency: any) => {
+              return (
+                <Box
+                  key={emergency.addr}
+                  maxW={{ base: 'none', sm: '75%', md: '60%', lg: '50%' }}
+                  my={6}
+                  p={5}
+                  borderRadius="md"
+                  bg="red.700"
+                >
+                  <EmergencyProposal emergency={emergency} />
                 </Box>
               );
             })}
