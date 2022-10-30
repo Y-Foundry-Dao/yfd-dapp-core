b730cb044624756ea395160a59681bdd2fe04ef8
Trenten <trenten.grede@gmail.com>
2022-10-30 02:01:22 -0500

    Fixed proposal msg to new proposal msg

--- src/utilities/messagesToEncode/msgEncodedProposal.tsx
+++ src/utilities/messagesToEncode/msgEncodedProposal.tsx
@@ -4,19 +4,18 @@ const msgEncodedProposal = (
   nameProposal: string,
   nameMsg: string,
   urlProposal: string,
+  nftAmount: number,
   tvlLimit: number,
   developmentCost: number,
   statementOfWork: string,
   paymentSchedule: number,
   github: string,
-  quorumPercent: number,
   selfVouchedInformation: string,
-  expiration: number,
   paymentFrequency: number,
   developer: string
 ) => {
   return `{
-  "create_proposal": {
+  "create_vault_proposal": {
     "msg": {
       "name": "${nameProposal}",
       "proposal_info": {
@@ -29,12 +28,11 @@ const msgEncodedProposal = (
         "developer": "${developer}",
         "development_cost": "${developmentCost}",
         "funding_denomination": "${YFD_TEST}",
+        "nft_quantity": ${nftAmount},
         "statement_of_work": "${statementOfWork}",
         "payment_schedule": ${Number(paymentSchedule)},
         "payment_frequency": ${paymentFrequency},
         "github": "${github}",
-        "expiration": ${expiration},
-        "quorum_percent": ${quorumPercent},
         "self_vouched_information": "${selfVouchedInformation}"
       }
     }
