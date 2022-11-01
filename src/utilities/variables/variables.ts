9cdb95a21833668343b4b55bbc1cf66a18421249
Ray Raspberry <ray@yfoundry.io>
2022-11-01 17:04:49 -0600

    updated Forge Contract Address

--- src/utilities/variables/variables.ts
+++ src/utilities/variables/variables.ts
@@ -11,11 +11,15 @@ export const YFD_TEST =
   'terra1ss9zz4873vk4dd8dvua0vm83m6s8k7ctwp9efac0arytn6jthfgsy2d4a9';
 export const FORGE_TEST_BROKEN =
   'terra168rvweaknfd7dhde02ex66fjsm9e3xppkj4xskvtnuql4vwr0ptsy444dx';
-export const FORGE_TEST_OLD =
+export const FORGE_TEST_OLDER =
   'terra1vd662ag5prc89szmdezsr8gythmhqp2tp6d7faeqpq8the349ymsmrwa87';
-export const FORGE_TEST =
+export const FORGE_TEST_OLD =
   'terra1ua4nwargz3udh5wlnu0l8knvymr4fsc0mnq426qz442f93zjrw5s63jcst';
+export const FORGE_TEST =
+  'terra1utkv5z538t32t0a2dkut2qncc4xs29jldjw2ez5s9tuzjsy59d0sm5p2mz';
 
+
+  
 // cronological data
 //export const CRONO_SECONDS_IN_TWO_YEAR = 31556952 * 2; // 63113904 // actual seconds
 export const CRONO_SECONDS_IN_TWO_WEEK = 60 * 60 * 24 * 7 * 2;
