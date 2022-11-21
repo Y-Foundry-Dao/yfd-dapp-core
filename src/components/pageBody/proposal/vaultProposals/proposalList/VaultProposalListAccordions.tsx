59e07341fc3380cf64bc87c38b0b0fc2f4ccbffa
Ray Raspberry <ray@yfoundry.io>
2022-11-20 23:52:08 -0600

    Updated Proposal Styles

--- src/components/pageBody/proposal/vaultProposals/proposalList/VaultProposalListAccordions.tsx
+++ src/components/pageBody/proposal/vaultProposals/proposalList/VaultProposalListAccordions.tsx
@@ -1,4 +1,4 @@
-import { Accordion } from '@chakra-ui/react';
+import { Accordion, list } from '@chakra-ui/react';
 import VaultProposalListAccordionItem from './VaultProposalListAccordionItem';
 import styles from 'styles/app.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,53 +8,22 @@ import {
   brands,
   icon
 } from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
-import { accordionAnatomy } from '@chakra-ui/anatomy';
-import { createMultiStyleConfigHelpers, defineStyle } from '@chakra-ui/react';
-
-const { definePartsStyle, defineMultiStyleConfig } =
-  createMultiStyleConfigHelpers(accordionAnatomy.keys);
-
-const custom = definePartsStyle({
-  panel: {
-    border: '1px solid',
-    borderColor: 'gray.200',
-    background: 'gray.50',
-    borderRadius: 'full',
-
-    // Let's also provide dark mode alternatives
-    _dark: {
-      borderColor: 'gray.600',
-      background: 'gray.800'
-    }
-  },
-  icon: {
-    border: '1px solid',
-    borderColor: 'gray.200',
-    background: 'gray.200',
-    borderRadius: 'full',
-    color: 'gray.500',
-
-    _dark: {
-      borderColor: 'gray.600',
-      background: 'gray.600',
-      color: 'gray.400'
-    }
-  }
-});
-
-export const accordionTheme = defineMultiStyleConfig({
-  variants: { custom }
-});
 
 function VaultProposalListAccordions({ proposals }: any) {
   return (
-    <Accordion w="100%" allowMultiple variant="custom">
+    <Accordion
+      w="100%"
+      allowMultiple
+      className={styles['proposal-list-wrapper-header']}
+      layerStyle="accordionWrapper"
+    >
       {proposals.map((proposal: any) => {
         return (
           <VaultProposalListAccordionItem
             key={proposal.addr}
             proposalContract={proposal.addr}
             proposalIndex={proposal.index}
+            className={styles['proposal-list-item']}
           />
         );
       })}
