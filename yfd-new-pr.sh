0d6671656e9fc6f36840d302e95f454ed4e606c8
Ray Raspberry <ray@yfoundry.io>
2022-11-30 23:59:07 -0600

    Updated variables into separate files.  Added Financing Proposal Type to Layout.  Added New Styled Progress Bar

--- yfd-new-pr.sh
+++ yfd-new-pr.sh
@@ -2,7 +2,9 @@
 # $1 = PR Title
 # $2 = PR Description and Commit message
 
-git branch -u=origin/main releases/v$1
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
+git branch -u=origin/main $branch
 git commit -a -m "$2"
 
 git push
