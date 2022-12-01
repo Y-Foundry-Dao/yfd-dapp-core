255b1ddb2224b5ac9946bf9c23e42841626d0959
Ray Raspberry <ray@yfoundry.io>
2022-12-01 02:07:20 -0600

    linked up external URLS and variables

--- src/utilities/variables/links.ts
+++ src/utilities/variables/links.ts
@@ -3,3 +3,5 @@ export const DISCORD = 'https://discord.gg/yfd';
 export const TELEGRAM = 'https://t.me/yfoundrydao';
 export const TWITTER = 'https://twitter.com/yieldfoundrydao';
 export const MEDIUM = 'https://medium.com/@yfoundry/';
+export const GITHUB = 'https://github.com/Y-Foundry-DAO/';
+export const DOCS = 'https://docs.yfoundry.io/';
