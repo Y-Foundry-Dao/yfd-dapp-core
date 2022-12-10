6eaec460e5f7e8ad55df37710794a33e84852b7d
Ray Raspberry <ray@yfoundry.io>
2022-12-09 22:06:22 -0600

    Main Navbar centered

--- src/features/nav/main/menu.tsx
+++ src/features/nav/main/menu.tsx
@@ -1,23 +1,32 @@
+import { Box, Flex, Spacer, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import styles from 'styles/app.module.scss';
+import styles from '@scss/app.module.scss';
 
 export default function MainMenu() {
   return (
     <div className={styles['main-header']}>
-      <a className={styles['menu-link-main']}>Sections</a>
       <div className={styles['header-menu']}>
-        <Link to="/" className={styles['main-header-link']}>
-          Dashboard
-        </Link>
-        <Link to="/vaults" className={styles['main-header-link']}>
-          Vaults
-        </Link>
-        <Link to="/initiatives" className={styles['main-header-link']}>
-          Initiatives
-        </Link>
-        <Link to="/proposals-governance" className={styles['main-header-link']}>
-          Governance
-        </Link>
+        <Flex>
+          <Spacer />
+          <Box>
+            <Link to="/" className={styles['main-header-link']}>
+              Dashboard
+            </Link>
+            <Link to="/vaults" className={styles['main-header-link']}>
+              Vaults
+            </Link>
+            <Link to="/initiatives" className={styles['main-header-link']}>
+              Initiatives
+            </Link>
+            <Link
+              to="/proposals-governance"
+              className={styles['main-header-link']}
+            >
+              Governance
+            </Link>
+          </Box>
+          <Spacer />
+        </Flex>
       </div>
     </div>
   );
