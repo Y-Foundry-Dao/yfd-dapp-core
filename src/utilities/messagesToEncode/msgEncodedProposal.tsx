53145a1fc1cb61f030c8a4e44b449af0b95fa434
Trenten <trenten.grede@gmail.com>
2022-12-29 14:59:40 -0600

    Updated the encoded message to make vault proposals from the UI
    Added ticker and fundingOnly properties, removed one unused name property, and renamed paymentSchedule to numberOfPayments

--- src/utilities/messagesToEncode/msgEncodedProposal.tsx
+++ src/utilities/messagesToEncode/msgEncodedProposal.tsx
@@ -2,24 +2,25 @@ import { YFD_TEST } from '@utilities/variables';
 
 const msgEncodedProposal = (
   nameProposal: string,
-  nameMsg: string,
+  ticker: string,
   urlProposal: string,
-  nftAmount: number,
   tvlLimit: number,
+  developer: string,
   developmentCost: number,
+  fundingOnly: boolean,
+  nftAmount: number,
   statementOfWork: string,
-  paymentSchedule: number,
-  github: string,
-  selfVouchedInformation: string,
+  numberOfPayments: number,
   paymentFrequency: number,
-  developer: string
+  github: string,
+  selfVouchedInformation: string
 ) => {
   return `{
   "create_vault_proposal": {
     "msg": {
-      "name": "${nameProposal}",
       "proposal_info": {
         "name": "${nameProposal}",
+        "ticker": "${ticker}",
         "proposal_url": "${urlProposal}",
         "tvl_limit": "${tvlLimit}",
         "contact": [
@@ -27,10 +28,11 @@ const msgEncodedProposal = (
         ],
         "developer": "${developer}",
         "development_cost": "${developmentCost}",
+        "funding_only": ${fundingOnly},
         "funding_denomination": "${YFD_TEST}",
         "nft_quantity": ${nftAmount},
         "statement_of_work": "${statementOfWork}",
-        "payment_schedule": ${Number(paymentSchedule)},
+        "num_payments": ${numberOfPayments},
         "payment_frequency": ${paymentFrequency},
         "github": "${github}",
         "self_vouched_information": "${selfVouchedInformation}"
