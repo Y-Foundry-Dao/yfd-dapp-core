70614c53ca2f9da6d947463691afff4f9bbd1a59
Trenten <trenten.grede@gmail.com>
2023-01-01 18:06:38 -0600

    Removed input handlers from useHandleInputs custom hook

--- src/hooks/useHandleInputs.tsx
+++ src/hooks/useHandleInputs.tsx
@@ -1,19 +1,6 @@
 import { useSetRecoilState } from 'recoil';
 import {
-  inputDeveloperWallet,
-  inputDevelopmentCost,
-  inputGithub,
-  inputInitialFunding,
-  inputNameProposal,
-  inputNFTAmount,
-  inputNumberOfPayments,
-  inputPaymentFrequency,
-  inputSelfVouchedInformation,
   inputStakeYFD,
-  inputStatementOfWork,
-  inputTicker,
-  inputTvlLimit,
-  inputUrlProposal,
   inputWhitelistWalletAddress,
   inputWhitelistWalletAddressGithub,
   inputWhitelistWalletAddressImageLink,
@@ -25,24 +12,7 @@ import {
 } from 'recoil/input/atoms';
 
 const useHandleInputs = () => {
-  const setSelfVouchedInformation = useSetRecoilState(
-    inputSelfVouchedInformation
-  );
-
   const setAmountStakeYFD = useSetRecoilState(inputStakeYFD);
-  // Recoil Values for vault proposal
-  const setNameProposal = useSetRecoilState(inputNameProposal);
-  const setUrlProposal = useSetRecoilState(inputUrlProposal);
-  const setTicker = useSetRecoilState(inputTicker);
-  const setTvlLimit = useSetRecoilState(inputTvlLimit);
-  const setDevelopmentCost = useSetRecoilState(inputDevelopmentCost);
-  const setStatementOfWork = useSetRecoilState(inputStatementOfWork);
-  const setGithub = useSetRecoilState(inputGithub);
-  const setNumberOfPayments = useSetRecoilState(inputNumberOfPayments);
-  const setPaymentFrequency = useSetRecoilState(inputPaymentFrequency);
-  const setInitialFunding = useSetRecoilState(inputInitialFunding);
-  const setNFTAmount = useSetRecoilState(inputNFTAmount);
-  const setDeveloper = useSetRecoilState(inputDeveloperWallet);
   // Recoil values for address whitelist
   const setWhitelistWalletAddress = useSetRecoilState(
     inputWhitelistWalletAddress
@@ -70,40 +40,6 @@ const useHandleInputs = () => {
   );
 
   const handleInputStakeYFD = (value: any) => setAmountStakeYFD(value);
-
-  const handleInputNameProposal = (event: any) =>
-    setNameProposal(event.target.value);
-
-  const handleInputTicker = (event: any) => setTicker(event.target.value);
-
-  const handleInputUrlProposal = (event: any) =>
-    setUrlProposal(event.target.value);
-
-  const handleInputTvlLimit = (value: any) => setTvlLimit(value);
-
-  const handleInputDevelopmentCost = (value: any) => setDevelopmentCost(value);
-
-  const handleInputInitialFunding = (value: any) => setInitialFunding(value);
-
-  const handleInputStatementOfWork = (event: any) =>
-    setStatementOfWork(event.target.value);
-
-  const handleInputNumberOfPayments = (value: any) =>
-    setNumberOfPayments(value);
-
-  const handleInputPaymentFrequency = (value: any) =>
-    setPaymentFrequency(value);
-
-  const handleInputGithub = (event: any) => setGithub(event.target.value);
-
-  const handleInputSelfVouchedInformation = (event: any) =>
-    setSelfVouchedInformation(event.target.value);
-
-  const handleInputNFTAmount = (value: any) => {
-    setNFTAmount(value);
-  };
-
-  const handleInputDeveloper = (event: any) => setDeveloper(event.target.value);
 
   const handleInputWhitelistWalletAddress = (event: any) =>
     setWhitelistWalletAddress(event.target.value);
@@ -134,19 +70,6 @@ const useHandleInputs = () => {
 
   return {
     handleInputStakeYFD,
-    handleInputNameProposal,
-    handleInputTicker,
-    handleInputUrlProposal,
-    handleInputTvlLimit,
-    handleInputDevelopmentCost,
-    handleInputInitialFunding,
-    handleInputStatementOfWork,
-    handleInputNumberOfPayments,
-    handleInputPaymentFrequency,
-    handleInputGithub,
-    handleInputSelfVouchedInformation,
-    handleInputNFTAmount,
-    handleInputDeveloper,
     handleInputWhitelistWalletAddress,
     handleInputWhitelistWalletAddressName,
     handleInputWhitelistWalletAddressImageLink,
