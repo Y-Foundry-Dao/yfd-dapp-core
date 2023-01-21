046e24a696b15c4ab1dec91bcde8c9854ad16f37
Ray Raspberry <ray@yfoundry.io>
2023-01-21 04:27:51 -0600

    Fixed Style Issues with Main Menu

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
         <Box className={styles.logo}>
           <Image src={yLogo} alt="Y-Foundry Logo" />
         </Box>
-        <Box className={styles['header-menu']}></Box>
+        <Box className={styles['menuHeader']}></Box>
         <Box className={styles['header-profile']}>
           {lockYFDMenuDisplay(status)}
           <WalletConnect />
