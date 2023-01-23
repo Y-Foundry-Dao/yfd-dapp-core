398462c7941a7f3d9317593967ae3a3fad9bdb16
Trenten <trenten.grede@gmail.com>
2023-01-23 15:23:13 -0600

    Capitalized Main's layout component name since it is a component

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,7 +2,7 @@ import styles from '@scss/wrapper.module.scss';
 import NavMain from '@features/nav/Main';
 import MainRoutes from '@routes/Main';
 
-export default function layoutMain() {
+export default function LayoutMain() {
   return (
     <div className={styles['main-container']}>
       <NavMain />
