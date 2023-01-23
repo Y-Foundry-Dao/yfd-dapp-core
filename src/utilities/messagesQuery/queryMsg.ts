bd116248c5748767f2587d2c4d560469a9b03de1
Trenten <trenten.grede@gmail.com>
2023-01-23 02:02:04 -0600

    Rebuilt queryMsg as a generic typed function

--- src/utilities/messagesQuery/queryMsg.ts
+++ src/utilities/messagesQuery/queryMsg.ts
@@ -4,25 +4,30 @@ function ConnectLCDClient() {
   return useLCDClient();
 }
 
-const queryMsg = async (contractAddress: string, msgQuery: object) => {
+const queryMsg = async <T>(
+  contractAddress: string,
+  msgQuery: object
+): Promise<T> => {
   const lcd = ConnectLCDClient();
   try {
     if (contractAddress) {
-      const queryResponse: any = await lcd.wasm.contractQuery(
+      const queryResponse = await lcd.wasm.contractQuery(
         contractAddress,
         msgQuery
       );
-      return queryResponse;
+      return queryResponse as T;
     }
   } catch (error: any) {
+    console.log('inside error in queryMsg function: ', error);
     if (
       error.response.data.message ===
       'cosmwasm_std::addresses::Addr not found: query wasm contract failed: invalid request'
     ) {
       // console.log({ addr: 'Contract Address Not Found' });
-      return { addr: 'Contract Address Not Found' };
+      return { addr: 'Contract Address Not Found' } as T;
     }
   }
+  return undefined as T;
 };
 
 export default queryMsg;
